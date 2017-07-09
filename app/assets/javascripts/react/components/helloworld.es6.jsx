4e184a0ab9ced29e44b6c9543349c9aca841ced7
Kamran Mirshahi <kamranmirshahi@Kamrans-MacBook-Pro.local>
2017-07-09 03:31:43 -0400

    First commit

---
+++ app/assets/javascripts/react/components/helloworld.es6.jsx
@@ -0,0 +1,20 @@
+class HelloWorld extends React.Component {
+
+function2() {
+alert('Congratulations we made it');
+}
+
+	render() {
+	return (
+	<div>
+	<p>Hello World </p>
+    <button style={{backgroundColor: 'red'}} onClick={this.function2}>Hello World </button>
+	</div>
+	);
+	}
+	}
+
+
+
+
+	
